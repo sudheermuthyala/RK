24c4fd121c8c55b9630aa8eb771b8207af071fb5
Muthyala <sudheer.muthyala@hp.com>
2023-02-21 16:23:55 +0530

    ---My Auto Git Commit ---

--- Python/python3.10.sh
+++ Python/python3.10.sh
@@ -1,4 +1,4 @@
-
+## Install python
 
 sudo apt update
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
